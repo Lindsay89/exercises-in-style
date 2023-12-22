21a28c770e13fb8a26786873d3ce893a90ffd3a5
Francesca Palermo <f.palermo@tecmasolutions.com>
2023-12-22 11:07:36 +0100

    perf: wip in sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
 import { DefaultProps } from "../../utils/types";
 import { Link } from "react-router-dom";
+import { Home, Image } from "react-feather";
+import { Button } from "../Button";
 
 // styles
 import "./sidebar.scss";
@@ -23,11 +25,23 @@ const defaultProps: SidebarOptionalProps = {
 };
 
 const Sidebar: React.FC<SidebarProps> = ({ className, ...rest }) => {
-  const classList = classNames("eis-sidebar", className);
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+  const classList = classNames("eis-sidebar", { isOpen }, className);
+
   return (
     <div className={classList} {...rest}>
-      <Link to={"/"}>Homepage</Link>
-      <Link to={"/Gallery"}>Gallery</Link>
+      <Button onClick={() => setIsOpen(!isOpen)} icon={"Menu"} />
+      <h1>Exercises in style</h1>
+      <div className="sidebar-content">
+        <Link to={"/"}>
+          <Home />
+          <span className="sidebar-link">Homepage</span>
+        </Link>
+        <Link to={"/Gallery"}>
+          <Image />
+          <span className="sidebar-link">Gallery</span>
+        </Link>
+      </div>
     </div>
   );
 };
